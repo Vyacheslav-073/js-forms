c16ed38a3c9956a396cf2acf89f6666b451510a7
Vyacheslav <viacheslav.suprun@yandex.ru>
2018-08-29 19:45:25 +0300

    login-check

--- js/check-comment.js
+++ js/check-comment.js
@@ -1,5 +1,39 @@
-$(document).ready(function() {
-
-	console.log('Hello world!');
-
+$(document).ready(function(){  
+    var commentFormCheck = (function(){ 
+        var _form = $('#comment-add-form');  
+        var _commentTextarea = $('#commentText');  
+        var _commentErrorEmpty = $('#commentErrorEmpty'); 
+ 
+        // Метод инициализации (запуска) модуля     
+        var init = function(){ 
+            _setUpListeners();// запускаем прослушку событий 
+            } 
+ 
+        // Метод прослушки событий  
+        // В нем прослушиваем события например клики по кнопке  
+        var _setUpListeners = function(){  
+            _form.on('submit', function(event){  
+                _formValidate(event);  
+            });  
+        }     
+ 
+    //  Приватные методы    
+    var _formValidate = function (event) {          
+        event.preventDefault();          
+        if ( _commentTextarea.val() == '' ){ 
+            _commentErrorEmpty.fadeIn(1000);  
+        } else {  
+            _commentErrorEmpty.fadeOut();  
+            $('form').unbind('submit').submit();  
+        }  
+    } 
+ 
+    // Возвращаем публичные методы, которые будут доступны снаружи    
+    return {  
+        init  
+        }  
+    }()); 
+ 
+    // Запускаем модуль  
+    commentFormCheck.init();     
 });
--- js/check-login.js
+++ js/check-login.js
@@ -1,5 +1,104 @@
-$(document).ready(function() {
+$(document).ready(function(){
+   (function() {
+		var formLoginValidation = {
+			isValid: true,
+			init: function(){
 
-	console.log('Hello world!');	
+            // вызов внутренних функций
+            this._setUpListeners();
+			},
+			_setUpListeners: function(){
+				$('#login-add-form').on('submit', formLoginValidation._validateForm);
+			},
 
+			_validateForm: (function(event){
+				event.preventDefault();
+				var form = $(this),
+					inputs = form.find('input'),
+					valid = true;
+
+				$.each(inputs, function(index, val){
+					var input = $(val),
+						value = input.val().trim(),
+                        valueEmail = $('input[type=email]').val().trim(),
+                        valuePassword = $('input[type=password]').val().trim(),
+                        emptyEmail = $('#empty-email'),
+                        correctEmail = $('#correct-email'),
+				        emptyPassword = $('#empty-password'),
+                        correctPassword = $('#correct-form'),
+                        crashLogin = $('#crash-login'),
+                        helpForm = $('#help-form'),
+                        email = $('input[name=email]'),
+					    password = $('input[name=email]'),
+						pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+
+				if (input.is('input:password')){
+				    if (value.length === 0){	
+						emptyPassword.fadeIn(1000);
+						valid = false;	
+					} else {
+						emptyPassword.fadeOut();	
+						}
+				    }	
+					if (!(input.is('input:password'))){                    
+				        if (valueEmail.length === 0){
+							emptyEmail.fadeIn(1000);
+							valid = false;
+						} else {
+							emptyEmail.fadeOut();
+						}
+				    }
+      					
+					// Проверка валидации email
+					if (!(input.is('input:password'))) {                        
+						if (input.attr('type').toLowerCase() === 'email') {
+							if (valueEmail !== '') {	 
+	      					    if (pattern.test(valueEmail)) {
+	  							   correctEmail.fadeOut();
+	      						} else {	
+									correctEmail.fadeIn(1000);
+	      							valid = false;
+	      						}
+							}
+						}
+					}
+				                    
+                    if (valueEmail !== "" && (pattern.test(valueEmail)) && valuePassword !== ""){                        
+                        if (valueEmail === "mail@mail.com" && valuePassword === 123){
+                            window.location.href='success.html';
+				        } else {
+                            helpForm.fadeIn(1000);
+				            crashLogin.fadeIn(1000);
+                            valid = false;
+				        }
+                    }
+                                        
+					input.on('focus', function(){						
+						if (input.is('input:password')){
+								emptyPassword.fadeOut();
+								correctPassword.fadeOut();
+							} else {
+								emptyEmail.fadeOut();
+								correctEmail.fadeOut();
+								correctPassword.fadeOut();
+							}
+					     });
+					
+					input.on('keydown', function(){
+						if (input.is('input:password')){
+							    emptyPassword.fadeOut();
+								correctPassword.fadeOut();
+							} else {
+								emptyEmail.fadeOut();
+								correctEmail.fadeOut();
+								correctPassword.fadeOut();
+							}
+				         });
+                    });
+			}),
+		};
+       
+        // Запускаем модуль       
+		formLoginValidation.init();
+	}());
 });
