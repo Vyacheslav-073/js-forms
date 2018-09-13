b112f2b2cc63e4abf24a5d07e8c5888666a098da
Vyacheslav <viacheslav.suprun@yandex.ru>
2018-09-13 19:04:22 +0300

    form-check

--- js/check-login.js
+++ js/check-login.js
@@ -1,5 +1,51 @@
-$(document).ready(function() {
-
-	console.log('Hello world!');	
-
-});
+$(document).ready(function(){
+    
+    $('#email').on('focus', function(){
+        $('#errorEmailEmpty').slideUp();
+        $('#errorValidEmail').slideUp();
+    });
+    
+    $('#password').on('focus', function(){
+        $('#errorPassEmpty').slideUp();
+    });
+   
+    $('#loginForm').on('submit', function(e){
+        e.preventDefault();
+        
+        var email = $('#email').val(),
+            password = $('#password').val(),
+            pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i,
+            validEmail = false,
+            validPass = false,
+            testEmail = 'mail@mail.com';
+        
+        // EMAIL не пустой
+        if (email == ''){
+            $('#errorEmailEmpty').slideDown();
+        } else {    
+            // EMAIL корректен?
+            if (pattern.test(email)){
+                validEmail = true;
+            } else {
+                $('#correctEmail').slideDown(); 
+            }
+        }
+        
+        // PASSWORD не пустой
+        if (password == ''){
+            $('#errorPassEmpty').slideDown();
+        } else {
+            validPass = true;
+        }
+        
+        if (validEmail == true){
+            if (email == testEmail){
+                $('#crashLogin.notify').show();
+                $('#crashLogin').slideDown();
+            } else {
+                $('#loginForm').unbind('submit');
+                $('#loginForm').submit();
+            }
+        }
+    });   
+});				
--- js/check-registration.js
+++ js/check-registration.js
@@ -1,5 +1,52 @@
-$(document).ready(function() {
-
-	console.log('Hello world!');
-
-});
+$(document).ready(function(){
+    
+    $('#email').on('focus', function(){
+        $('#errorEmailEmpty').slideUp();
+        $('#correctEmail').slideUp();
+        $('#crashLogin').slideUp();
+    });
+    
+    $('#password').on('focus', function(){
+        $('#errorPassEmpty').slideUp();
+    });
+   
+    $('#formRegister').on('submit', function(e){
+        e.preventDefault();
+        
+        var email = $('#email').val(),
+            password = $('#password').val(),
+            pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i,
+            validEmail = false,
+            validPass = false,
+            testEmail = 'mail@mail.com';
+        
+        // EMAIL не пустой
+        if (email == ''){
+            $('#errorEmailEmpty').slideDown();
+        } else {    
+            // EMAIL корректен?
+            if (pattern.test(email)){
+                validEmail = true;
+            } else {
+                $('#correctEmail').slideDown(); 
+            }
+        }
+        
+        // PASSWORD не пустой
+        if (password == ''){
+            $('#errorPassEmpty').slideDown();
+        } else {
+            validPass = true;
+        }
+            
+        if (validEmail == true  && validPass == true){
+            if (email == testEmail){
+                $('#crashLogin.notify').show();
+                $('#crashLogin').slideDown();
+            } else {
+                s('#formRegister').unbind('submit');
+                s('#formRegister').submit();
+            } 
+        }    
+    });   
+});	
